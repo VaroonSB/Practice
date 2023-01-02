a111f05aa6be9ae2a2962c2b1ee6bab5d21c8e85
VaroonSB <varoonsb1311@gmail.com>
2023-01-02 14:52:44 +0530

    fix: FibonacciSeries

--- first_java/FibonacciSeries.java
+++ first_java/FibonacciSeries.java
@@ -6,13 +6,13 @@ public class FibonacciSeries {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int limit = input.nextInt();
-    int first = 1;
+    int first = 0;
     int second = 1;
-    System.out.println(0 + " ");
     for (int i = 0; i < limit; i++) {
       System.out.println(first + " ");
+      int sum = first + second;
       first = second;
-      second = first + second;
+      second = sum;
     }
     input.close();
   }
