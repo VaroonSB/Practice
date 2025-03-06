21561beda0b3daa86e84731c4bc2a797cb193ce4
VaroonSB <varoonsb1311@gmail.com>
2025-03-06 20:56:44 +0530

    bubble sort

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean isSwap = false;
+            for (int j = 1; j < nums.length - i; j++) {
+                if (nums[j] < nums[j-1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                    isSwap = true;
+                }
+            }
+            if (!isSwap) {
+                break;
+            }
+        }
+    }
+}
