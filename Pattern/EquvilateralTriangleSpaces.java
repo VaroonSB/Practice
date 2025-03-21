182aeb463eef874416ba893e917543f0f6344038
VaroonSB <varoonsb1311@gmail.com>
2025-03-21 18:30:37 +0530

    equilateral triangle with spaces print first approach

--- Pattern/EquvilateralTriangleSpaces.java
+++ Pattern/EquvilateralTriangleSpaces.java
@@ -16,3 +16,21 @@ class Main {
         }
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            int spaces = n - i;
+
+            for (int k = 1; k < spaces; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
