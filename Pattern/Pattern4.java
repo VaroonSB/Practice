1f335720194f18e5cda15cc6967a99d74ebd8389
VaroonSB <varoonsb1311@gmail.com>
2025-03-24 10:18:41 +0530

    Pattern 4

--- Pattern/Pattern4.java
+++ Pattern/Pattern4.java
@@ -1,8 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print(j+1 + " ");
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
             }
             System.out.println("");
         }
