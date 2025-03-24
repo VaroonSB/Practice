2801cc218d1d9688cc7487445a24d754c585887c
VaroonSB <varoonsb1311@gmail.com>
2025-03-24 10:26:17 +0530

    Pattern 5

--- Pattern/Pattern5.java
+++ Pattern/Pattern5.java
@@ -1,9 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        int n = 4;
-        for (int i = 0; i < 2*n+1; i++) {
-            int col = i < n ? i : 2*n-i;
-            for (int j = 0; j <= col; j++) {
+        int n = 5;
+
+        for (int i = 1; i <= 2*n; i++) {
+            int col = i <= n ? i : (2*n) - i;
+            for (int j = 1; j <= col; j++) {
                 System.out.print("* ");
             }
             System.out.println("");
