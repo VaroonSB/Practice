e25d5747c933e0b7cf4e29dd5ad6a4887378efb2
VaroonSB <varoonsb1311@gmail.com>
2025-03-26 21:02:51 +0530

    Pattern 17

--- Pattern/Pattern17.java
+++ Pattern/Pattern17.java
@@ -1,22 +1,24 @@
 class Main {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 1; i <= 2*n; i++) {
+
+        for (int i = 1; i <= 2 * n; i++) {
+
             int spaces = i <= n ? n - i : i - n;
-
-            for (int k = 1; k <= spaces; k++) {
-                System.out.print("  ");
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
             }
 
-            int col = i < n ? i : 2*n - i;
-            for (int k = col; k >= 1; k--) {
-                System.out.print(k+" ");
-            }
-            for (int k = 2; k <= col; k++) {
-                System.out.print(k+" ");
+            int col = i <= n ? i : 2 * n - i;
+            for (int j = col; j >= 1; j--) {
+                System.out.print(j);
             }
 
-            System.out.println("");
+            for (int j = 2; j <= col; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
         }
     }
 }
