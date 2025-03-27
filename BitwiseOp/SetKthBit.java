50ce56b0d9ddbe40006b39d51c1224136d132635
VaroonSB <varoonsb1311@gmail.com>
2025-03-27 11:46:52 +0530

    reset kth bit in binary number

--- BitwiseOp/SetKthBit.java
+++ BitwiseOp/SetKthBit.java
@@ -2,4 +2,8 @@ class Solution {
     static int setKthBit(int n, int k) {
         return n | (1 << (k));
     }
+
+    static int resetKthBit(int n, int k) {
+        return n & (1 << (k));
+    }
 }
