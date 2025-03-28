92e95901b265ebb598af681be308cd9c050a15cd
VaroonSB <varoonsb1311@gmail.com>
2025-03-28 16:15:13 +0530

    fix find kth bit

--- BitwiseOp/FindkthBit.java
+++ BitwiseOp/FindkthBit.java
@@ -5,6 +5,8 @@ class Solution {
      * For that, left shift 1 by k-1 times for trialing 0s.
      */
     public int singleNumber(int num, int k) {
-        return num & (1 << (k - 1));
+        int result = num & (1 << (k - 1));
+
+        return result == 0 ? 0 : 1;
     }
 }
