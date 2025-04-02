d9c3942653a2b543c9b0031e01524e445e13d720
VaroonSB <varoonsb1311@gmail.com>
2025-04-02 20:37:20 +0530

    reverse a number - approach 2 using digits using recursion

--- Recursion/ReverseNumber.java
+++ Recursion/ReverseNumber.java
@@ -11,3 +11,19 @@ class Main {
         return reverse(n/10, reversed * 10 + n%10);
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(reverse(23314));
+    }
+
+    static int reverse(int n) {
+        if (n % 10 == n) {
+            return n;
+        }
+
+        int digits = (int)(Math.log10(n)) + 1;
+
+        return n%10 * (int)Math.pow(10, digits-1) + reverse(n/10);
+    }
+}
