bef3295f96182ebb474309ef8fb83361d3d359a8
VaroonSB <varoonsb1311@gmail.com>
2025-04-02 17:55:51 +0530

    print asc using recursion

--- Recursion/Print.java
+++ Recursion/Print.java
@@ -11,3 +11,17 @@ class Main {
         print(n-1);
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        print(10, 1);
+    }
+
+    static void print(int n, int i) {
+        if (i > n) {
+            return;
+        }
+        System.out.println(i + " ");
+        print(n, i+1);
+    }
+}
