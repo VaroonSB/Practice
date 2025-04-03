f2509da56c0ef5218f42fd8b8737ebb8d149bcb9
VaroonSB <varoonsb1311@gmail.com>
2025-04-03 20:42:12 +0530

    linear multi search using fn body variable recursion

--- Recursion_Arrays/LinearMultiSearch.java
+++ Recursion_Arrays/LinearMultiSearch.java
@@ -22,3 +22,31 @@ class Main {
         linearMultiSearch(arr, i+1, target);
     }
 }
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println(linearMultiSearch(new int[]{4, 10, 8, 9, 8, 10}, 0, 8));
+    }
+
+    static ArrayList linearMultiSearch(int[] arr, int i, int target) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if (i == arr.length) {
+            return list;
+        }
+
+        if (arr[i] == target) {
+            list.add(i);
+        }
+
+        ArrayList<Integer> stackList = linearMultiSearch(arr, i+1, target);
+
+        list.addAll(stackList);
+
+        return list;
+    }
+}
