a44e0a6e9db787f6c09ab4b3d681e6e33dff63ed
VaroonSB <varoonsb1311@gmail.com>
2025-04-07 14:13:45 +0530

    inverted triangle-2 using recursion

--- Recursion_Patterns/InvertedTriangle.java
+++ Recursion_Patterns/InvertedTriangle.java
@@ -22,3 +22,24 @@ class Main {
     }
 
 }
+
+class Main {
+    public static void main(String[] args) {
+        print(4, 1);
+    }
+
+    static void print(int row, int col) {
+
+        if(row == 0) {
+            return;
+        }
+
+        if (col <= row) {
+            System.out.print("*");
+            print(row, col+1);
+        } else {
+            System.out.println();
+            print(row-1, 1);
+        }
+    }
+}
