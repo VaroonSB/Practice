812769fe9d9b1c2bebd1c20ef6645e2525ddd63b
VaroonSB <varoonsb1311@gmail.com>
2025-04-14 12:53:36 +0530

    skip a character using substring()

--- Recursion_subsets/SkipAChar.java
+++ Recursion_subsets/SkipAChar.java
@@ -16,3 +16,24 @@ class Main {
         return str.charAt(i) + skipAChar(str, i+1);
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        System.out.print(skipAChar("", "basfbacab"));
+    }
+
+    static String skipAChar(String p, String unp) {
+
+        if (unp.isEmpty()) {
+            return p;
+        }
+
+        char ch = unp.charAt(0);
+
+        if (ch == 'a') {
+            return skipAChar(p, unp.substring(1));
+        } else {
+            return skipAChar(p+ch, unp.substring(1));
+        }
+    }
+}
