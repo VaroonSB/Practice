500bdc83555ea0eba1d60d896cac4aef1f912a52
VaroonSB <varoonsb1311@gmail.com>
2025-04-14 15:23:09 +0530

    print subsequences string, left and right.

--- Recursion_subsets/Subsequence.java
+++ Recursion_subsets/Subsequence.java
@@ -17,3 +17,28 @@ class Main {
         subsequence(p, unp.substring(1));
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        System.out.print(subsequence("", "abc"));
+    }
+
+    static ArrayList<String> subsequence(String p, String unp) {
+        if (unp.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            if (!p.isEmpty()) {
+                list.add(p);
+            }
+            return list;
+        }
+
+        char ch = unp.charAt(0);
+
+        ArrayList<String> left = subsequence(p+ch, unp.substring(1));
+        ArrayList<String> right = subsequence(p, unp.substring(1));
+
+        left.addAll(right);
+
+        return left;
+    }
+}
