4cf177ad3f40012bf9a297de03b467d297ecaf75
VaroonSB <varoonsb1311@gmail.com>
2025-04-15 18:51:32 +0530

    L-17 Letter combinations working solution

--- Recursion_subsets/LetterCombinations.java
+++ Recursion_subsets/LetterCombinations.java
@@ -23,3 +23,41 @@ class Solution {
         }
     }
 }
+
+class Solution {
+
+    static Map<Integer, String> letterMap = Map.of(
+            2, "abc", 3, "def", 4, "ghi", 5, "jkl",
+            6, "mno", 7, "pqrs", 8, "tuv", 9, "wxyz"
+    );
+
+    public List<String> letterCombinations(String digits) {
+        return combinations("", digits);
+    }
+
+    static List<String> combinations(String p, String up) {
+        if (up.isEmpty()) {
+            List<String> list = new ArrayList<>();
+            if (!p.isEmpty()) {
+                list.add(p);
+            }
+            return list;
+        }
+
+        int digit = up.charAt(0) - '0';
+
+        String letters = letterMap.get(digit);
+        System.out.print(letters);
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < letters.length(); i++) {
+
+            char ch = letters.charAt(i);
+
+            result.addAll(combinations(p+ch, up.substring(1)));
+        }
+
+        return result;
+    }
+}
