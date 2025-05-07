437e4ea054ca8a938b6cac7ea2b973a36d4047d2
VaroonSB <varoonsb1311@gmail.com>
2025-05-07 20:47:05 +0530

    cycle length in linked list

---
+++ LinkedList/LoopLength.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int cycleLength(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                int count = 0;
+                do {
+                    slow = slow.next;
+                    i++;
+                } while (slow != fast);
+                return count;
+            }
+        }
+
+        return 0;
+    }
+}
