c3324a0d030aab3047c9b5a88cf61af4f1d9e119
VaroonSB <varoonsb1311@gmail.com>
2025-05-12 22:12:58 +0530

    in place reversal of linked list L-206

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -28,3 +28,35 @@ class Solution {
         return temp;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = head.next;
+
+        while (current != null) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        return prev;
+    }
+}
