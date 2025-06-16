314dfeb7847b4b350b424c175882a0fc020833c8
VaroonSB <varoonsb1311@gmail.com>
2025-06-16 09:57:57 +0530

    ReverseKNodes best solution

--- LinkedList/ReverseKNodes.java
+++ LinkedList/ReverseKNodes.java
@@ -61,3 +61,71 @@ class ReverseKNodes {
         return head;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k == 1 || head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode start = head, end = head;
+        ListNode dummy = new ListNode(0, head); // use to return the head at last
+        ListNode prev = dummy;
+
+        while (true) {
+
+            // traverse till next k nodes
+            for (int i = 1; i < k && end != null; i++) {
+                end = end.next;
+            }
+
+            // we have hit the end of the list
+            if (end == null) {
+                break;
+            }
+
+            // store next start and the connector for start
+            ListNode next = end.next;
+
+            // break the end. connect later
+            end.next = null;
+
+            ListNode revHead = reverse(start);
+
+            // connections
+            prev.next = revHead; // left side
+            start.next = next; // right side
+
+            // set stage for next set of k nodes
+            prev = start;
+            start = prev.next;
+            end = prev.next;
+        }
+
+        return dummy.next; // peace
+    }
+
+    ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+
+        return prev;
+    }
+}
