bb58fb5d4b0faa6b9a7556db959fce0d762f7fdb
VaroonSB <varoonsb1311@gmail.com>
2025-07-12 18:28:49 +0530

    Diameter of tree L-543

---
+++ Tree/Problems/DFS/L-543 Diameter of Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = Integer.MIN_VALUE;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return max - 1;
+    }
+
+    int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int diameter = leftHeight + rightHeight + 1;
+        max = Math.max(diameter, max);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
