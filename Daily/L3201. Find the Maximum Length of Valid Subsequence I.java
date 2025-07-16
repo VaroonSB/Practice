3f7f899486d0a122803a1abde79eb2e8eee4dba3
VaroonSB <varoonsb1311@gmail.com>
2025-07-16 06:49:53 +0530

    3201. Find the Maximum Length of Valid Subsequence I - dp solution

--- Daily/L3201. Find the Maximum Length of Valid Subsequence I.java
+++ Daily/L3201. Find the Maximum Length of Valid Subsequence I.java
@@ -39,3 +39,34 @@ class Solution {
         return max;
     }
 }
+
+
+// I don't understand this now
+
+class Solution {
+    public int maximumLength(int[] nums) {
+        // dp[sum_parity][end_parity]
+        int[][] dp = new int[2][2];
+
+        for (int num : nums) {
+            int p = num % 2; // current parity
+
+            // Case 1: EVEN sum (e.g., even+even, odd+odd)
+            // The previous element must have the same parity 'p'.
+            dp[0][p] = dp[0][p] + 1;
+
+            // Case 2: ODD sum (e.g., even+odd, odd+even)
+            // The previous element must have the opposite parity '1-p'.
+            dp[1][p] = dp[1][1 - p] + 1;
+        }
+
+        // The result is the maximum value found in our DP table.
+        int res = 0;
+        res = Math.max(res, dp[0][0]);
+        res = Math.max(res, dp[0][1]);
+        res = Math.max(res, dp[1][0]);
+        res = Math.max(res, dp[1][1]);
+
+        return res;
+    }
+}
