2313e5e98d553a58be17b3e0b4be4f28c5f67732
VaroonSB <varoonsb1311@gmail.com>
2025-07-20 15:54:57 +0530

    Construct using preorder and inorder using hashmap for index L-105

--- Tree/Problems/DFS/L-105 Construct using preorder and inorder.java
+++ Tree/Problems/DFS/L-105 Construct using preorder and inorder.java
@@ -43,3 +43,54 @@ class Solution {
         return index;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int index = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return helper(preorder, inorder, 0, inorder.length - 1, map);
+    }
+
+    TreeNode helper(int[] preorder, int[] inorder, int left, int right, HashMap<Integer, Integer> map) {
+        if (left > right) {
+            return null;
+        }
+
+        int current = preorder[index];
+        index++;
+
+        TreeNode node = new TreeNode(current);
+
+        if (left == right) {
+            return node;
+        }
+
+        int inorderIndex = map.get(current);
+
+        node.left = helper(preorder, inorder, left, inorderIndex - 1, map);
+        node.right = helper(preorder, inorder, inorderIndex + 1, right, map);
+
+        return node;
+    }
+}
