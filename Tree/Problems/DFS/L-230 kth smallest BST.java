541b957a761100e0f8104074fa49e915bc550e35
VaroonSB <varoonsb1311@gmail.com>
2025-07-21 09:09:55 +0530

    kth smallest BST using heap L-230

--- Tree/Problems/DFS/L-230 kth smallest BST.java
+++ Tree/Problems/DFS/L-230 kth smallest BST.java
@@ -39,3 +39,88 @@ class Solution {
         return solution(node.right, k);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result;
+    int k;
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        inorder(root);
+        return result;
+
+    }
+
+    void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+
+        k--;
+        if (k == 0) {
+            result = node.val;
+            return;
+        }
+
+        inorder(node.right);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    PriorityQueue<Integer> minHeap;
+    public int kthSmallest(TreeNode root, int k) {
+        minHeap = new PriorityQueue<>();
+
+        inorder(root);
+
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+            result = minHeap.poll();
+        }
+
+        return result;
+    }
+
+    void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+
+        minHeap.offer(node.val);
+
+        inorder(node.right);
+    }
+}
